0fd8014cd004a29c8a3d7ab45a4154c3e31511a0
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-09-04 02:46:27 +0530

    made the search bar with icons and connected firebase DB to display data on frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi"
+import { AiFillPlusCircle } from "react-icons/ai"
+import { HiOutlineUserCircle } from "react-icons/hi"
+import { RiEditCircleLine } from "react-icons/ri"
+import { IoMdTrash } from "react-icons/io"
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from "./config/firebase";
 
 const App = () => {
-  return <div className="bg-red-700 flex-1">App</div>;
+
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        })
+        setContacts(contactLists)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getContacts();
+  }, [])
+
+
+  return (
+    <div className="max-w-[370px] mx-auto px-4">
+      <Navbar />
+      <div className="flex gap-2">
+        <div className="flex realative items-center flex-grow">
+          <FiSearch className="ml-1 absolute text-white text-3xl" />
+          <input type="text" className="pl-9 text-white flex-grow h-10 bg-transparent border border-white rounded-md" />
+        </div>
+        <AiFillPlusCircle className="text-5xl text-white cursor-pointer" />
+      </div>
+
+      <div>
+        {
+          contacts.map((contact) =>
+            <div key={contact.id}>
+              <HiOutlineUserCircle />
+              <div className="text-white">
+                <h2 className="">{contact.name}</h2>
+                <p className="">{contact.email}</p>
+              </div>
+              <div>
+                <RiEditCircleLine />
+                <IoMdTrash />
+              </div>
+            </div>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return <div className="flex justify-center items-center h-[60px] bg-white gap-2 my-4 rounded-lg text-xl font-medium">
+    <img src="firebase.svg" alt="" />
+    <h1>Firebase Contact App</h1>
+  </div>;
+};
+
+export default Navbar;
